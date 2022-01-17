b07ac5be65ccfaefb082bc76115d0c6e76744d9b
Rebecca <njokinyagah@gmail.com>
2022-01-17 20:40:49 +0300

    updated readme page

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,56 +1,78 @@
 
-
+//function to toggle between the icons and the text
 $(document).ready(function(){
     
-  $(".btn_1").click(function(){ 
-      $("p.par_1").show();
-      $(".btn_1").hide(); 
+    $(".btn_1").click(function(){ 
+        $("p.par_1").show();
+        $(".btn_1").hide(); 
+    });
+    $("p.par_1").click(function(){
+        $("p.par_1").hide();
+        $(".btn_1").show(); 
+    });
+     $(".btn_2").click(function(){
+         $("p.par_2").show();   
+         $(".btn_2").hide();
+     });
+     $("p.par_2").click(function(){
+         $("p.par_2").hide();
+         $(".btn_2").show();
+     });
+     $(".btn_3").click(function(){
+         $("p.par_3").show();   
+         $(".btn_3").hide();
+     });
+     $("p.par_3").click(function(){
+         $(".btn_3").show();
+         $("p.par_3").hide();
+     }); 
   });
-  $("p.par_1").click(function(){
-      $("p.par_1").hide();
-      $(".btn_1").show(); 
-  });
-   $(".btn_2").click(function(){
-       $("p.par_2").show();   
-       $(".btn_2").hide();
-   });
-   $("p.par_2").click(function(){
-       $("p.par_2").hide();
-       $(".btn_2").show();
-   });
-   $(".btn_3").click(function(){
-       $("p.par_3").show();   
-       $(".btn_3").hide();
-   });
-   $("p.par_3").click(function(){
-       $(".btn_3").show();
-       $("p.par_3").hide();
-   }); 
+ 
+
+$(document).ready(function(){
+    $("form").submit(function(){
+        event.preventDefault();
+            var name = $('#inputName').val;
+            var email = $('#inputEmail').val;
+            var message = $('#inputMessage').val
+          if (!name.value || !email.value || !message.value){
+              alert('Please check your entries');
+          } else{
+              event.preventDefault();
+              $(this).get(0).reset();
+              alert("successful submission");
+          }
+        //  alert("submition");
+       
+    });
+    $(".col-sm-3 .box").hide(500);
+    $(".col-sm-3").hover(function(){
+        $(this).children(".col-sm-3 .box").toggle(500);
+    });
+
 });
 
 
-$(document).ready(function(){
-  $("form").submit(function(){
-      event.preventDefault();
-          var name = $('#inputName').val;
-          var email = $('#inputEmail').val;
-          var message = $('#inputMessage').val
-        if (!name.value || !email.value || !message.value){
-            alert('Please check your entries');
-        } else{
-            event.preventDefault();
-            $(this).get(0).reset();
-            alert("successful submission");
-        }
-    
-     
-  });
-  $(".col-sm-3 .box").hide(500);
-  $(".col-sm-3").hover(function(){
-      $(this).children(".col-sm-3 .box").toggle(500);
-  });
+// function for the email validation
 
-});
+//  $(document).ready(function(){
+//          $(".submit").click(fuction(){
+//                    event.preventDefault();
+//                    console.log("Clicked button");
+        
+//                    var email = $("email").val();
+//                    var name = $("name").val();
+//                    var message = $("message").val();
+//                    Var statusElm = $("status");
+        
+//                    if(email.length > 5 && email.include("@") && email.include("."){
+//                        console.log("Email is valid");
+//                    });
+//                     else {
+//                       console.log("Email is not valid");
+//                   }
+//                });
+//        });
 
 
 
