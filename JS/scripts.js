7bcd56a1030d2d7b05ceef3990657c1c4e7d7edd
Rebecca <njokinyagah@gmail.com>
2022-02-01 17:33:34 +0300

    updated js

--- JS/scripts.js
+++ JS/scripts.js
@@ -2,9 +2,9 @@
 $(document).ready(function(){
     
 
-  $(".btn_2").click(function(){
-      $(".btn_2").hide();
-      $(".btn_2").show(); 
+  $(".btn_1").click(function(){
+      $(".p.par_1").hide();
+      $(".btn_1").show(); 
   });
    $(".btn_2").click(function(){
        $("p.par_2").show();   
@@ -15,7 +15,7 @@ $(document).ready(function(){
        $(".btn_2").show();
    });
    $(".btn_3").click(function(){
-       $("p.par_3").show();   
+       $("p.p3").show();   
        $(".btn_3").hide();
    });
    $("p.par_3").click(function(){
