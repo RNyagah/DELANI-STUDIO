2cff78332510ce4528b4943f59431786dbf48618
Rebecca <njokinyagah@gmail.com>
2022-02-02 12:46:36 +0300

    updated js

--- JS/scripts.js
+++ JS/scripts.js
@@ -2,15 +2,15 @@
 $(document).ready(function(){
     
 
-  $(".col-md-4").click(function(){
+  $(".des-img").click(function(){
       $(".p.par_1").hide();
       $(".btn_1").show(); 
   });
-   $(".btn_2").click(function(){
+   $(".dev-img").click(function(){
        $("p.par_2").show();   
        $(".btn_2").hide();
    });
-   $(".p.pa2").click(function(){
+   $(".prod-img").click(function(){
        $("p.par_2").hide();
        $(".btn_2").show();
    });
